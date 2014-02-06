5510b38449cf2352ac62e4f28307a7b82009686a
asmatameem <atameem@spokeo.com>
2014-02-05 17:29:26 -0800

    fixed marshalling issue

--- lib/serializable_attributes/attributes.rb
+++ lib/serializable_attributes/attributes.rb
@@ -5,9 +5,11 @@ module SerializableAttributes
   # The data doesn't need to be marshalled because it can be retrieved from the raw data.
   class Attributes < ::Hash
     def marshal_dump
+      ::Hash[to_a]
     end
 
     def marshal_load(data)
+      merge!(data)
     end
   end
 end
